bf6c66c4731456d09e096175cd7704b2b2a43617
root <root@ELC-D2R4WT3.ap.elcompanies.net>
2025-08-01 17:22:09 +0800

    update homepage

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello from Azure DevOps!'));
+app.get('/', (req, res) => res.send('Hello from Azure DevOps version 2!'));
 app.listen(port, () => console.log(`App running on port ${port}`));
 
